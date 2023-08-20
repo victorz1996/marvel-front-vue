a87c69414dd843331c6d71fa09aefcb2910021d8
Victor Zabala <victor.zabala@interedes.com.co>
2023-08-20 17:48:09 -0500

    init history viwe

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -12,9 +12,41 @@ export default {
 }
 </script>
 <template>
-  <div class="about">
-    <h1>This is an history page</h1>
-  </div>
+  <div class="tabs">
+      <TabComponent
+        v-if="buildDataToTabs(serie).length"
+        @changeTab="getResourcesBySerieIdAndResourceName(serie.id, $event)"
+        :data-to-tabs="buildDataToTabs(serie)"
+      >
+        <div>
+          <div
+            style="display: flex; margin-top: 1rem"
+            v-for="resource of detailResourcesList"
+            :key="resource.id"
+          >
+            <img
+              v-if="resource.thumbnail"
+              width="200"
+              height="200"
+              style="min-width: 200px"
+              :src="resource.thumbnail?.path + '.' + resource.thumbnail?.extension"
+              alt=""
+            />
+            <div style="margin-left: 4rem">
+              <h4 v-if="resource.fullName || resource.name">
+                Name: {{ resource.fullName || resource.name }}
+              </h4>
+              <h4 v-if="resource.title">Title: {{ resource.title }}</h4>
+              <p>Modified: {{ resource.modified }}</p>
+              <p v-if="resource.textObjects?.length">
+                Description: {{ resource.textObjects[0].text }}
+              </p>
+              <p v-if="activeResource === 'Stories'">Type: {{ resource.type }}</p>
+            </div>
+          </div>
+        </div></TabComponent
+      >
+      <div v-else>Not found resources</div>
 </template>
 
 <style></style>
